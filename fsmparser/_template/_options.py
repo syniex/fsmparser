6efd930142d9ebfe37d21f534a2bff7a6caf7ca0
Tomer Manzur <90573138+t-manzur@users.noreply.github.com>
2023-04-11 13:42:38 +0000

    Implement more FSM Options

--- fsmparser/_template/_options.py
+++ fsmparser/_template/_options.py
@@ -47,3 +47,63 @@ class FSMBaseOption:
 
     def clear_all(self) -> None:
         return None
+
+
+class Key(FSMBaseOption):
+    ...
+
+
+class Required(FSMBaseOption):
+    """The Value must be non-empty for the row to be recorded."""
+
+    def save(self) -> None:
+        if self.value._value is None:
+            self.value.clear()
+            raise SkipRecord
+
+
+class List(FSMBaseOption):
+    def create(self) -> None:
+        self._value = []
+
+    def clear(self) -> None:
+        if 'Filldown' not in self.value.options:
+            self._value = []
+
+    def clear_all(self) -> None:
+        self._value = []
+
+    def assign(self) -> None:
+        match = self.value.compiled_regex.match(self.value.value) if self.value.compiled_regex.groups > 1 else None
+        if match and match.groupdict():
+            self._value.append(match.groupdict())
+        else:
+            self._value.append(self.value.value)
+
+    def save(self):
+        self.value._value = list(self._value)
+
+
+class Fillup(FSMBaseOption):
+    def assign(self) -> None:
+        if self.value._value is None:
+            return
+        index = list(self.value._parent._values.values()).index(self.value)
+        for result in reversed(self.value._parent._results):
+            if result[index] is not None:
+                break
+            result[index] = self.value._value
+
+
+class Filldown(FSMBaseOption):
+    def create(self) -> None:
+        self._value: 'str | None' = None
+
+    def assign(self) -> None:
+        self._value = self.value.value
+
+    def clear(self) -> None:
+        self.value._value = self._value
+
+    def clear_all(self) -> None:
+        self._value = None
